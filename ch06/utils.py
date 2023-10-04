5f3de6548a35a664f3a01eefd56f83704f9446df
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-04 14:17:37 +0900

    Update utils.py

--- ch06/utils.py
+++ ch06/utils.py
@@ -9,7 +9,6 @@ def draw_fruits (fruits):
     fig, axs = plt.subplots((len(fruits) // 30) + 1, 30, squeeze = False)
     for i, fruit in enumerate(fruits):
         fruit = fruit.reshape(100, 100)
-        print(i, (len(fruits) // 30) + 1, 30)
         axs[i // 30][i % 30].imshow(fruit)
         axs[i // 30][i % 30].axis('off')
     plt.show()
